385434f5fdf8c7b5bfbe9d22aa933883b557a2ab
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-11-28 22:24:47 -0800

    Fixed the header-fetching script.

--- package/fetch_headers.sh
+++ package/fetch_headers.sh
@@ -8,13 +8,13 @@ fi
 # Transferring header files.
 cd _cpp
 git checkout b6dbb52353c5d2a2ffaa94ecaf3a811bf4480b31
-cp -r include/kmeans ../src
+cp -r include/kmeans ../inst/include
 
 # Fetching dependencies.
 cmake -B build -S .
-cp -r build/_deps/aarand-src/include/aarand ../src
+cp -r build/_deps/aarand-src/include/aarand ../inst/include
 
 # Committing everything.
 cd ..
-git add src/kmeans
-git add src/aarand
+git add inst/include/kmeans
+git add inst/include/aarand
